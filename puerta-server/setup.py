5b88ba251576521958607da62cf5bf80eae4eeb9
João Mesquita <jmesquita@indicium.com.ar>
2015-07-19 19:05:17 -0300

    Fix requrires.

--- puerta-server/setup.py
+++ puerta-server/setup.py
@@ -39,7 +39,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['sqlalchemy, flask, flask-restful, flask-sqlalchemy, python-bcrypt'],
+    install_requires=['sqlalchemy', 'flask', 'flask-restful', 'flask-sqlalchemy', 'python-bcrypt'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
